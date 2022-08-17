cfb07aa5e82d7bf92671d38fa62e34109863b137
jenny <eveydemann@gmail.com>
2022-08-17 17:11:41 +0300

    MyHomeworkTests modified

--- 08_XPath/src/test/java/MyHomeworkTests.java
+++ 08_XPath/src/test/java/MyHomeworkTests.java
@@ -5,7 +5,33 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyHomeworkTests {
 
-    /* В этом классе можете описать тесты для домашнего задания */
+    // задание 1
+
+    var firstLocator = By.xpath("//*[@class='da-arrows-prev']");
+    var secondLocator = By.xpath("//a[@href='#'][@class='da-link button']");
+    var thirdLocator = By.xpath("//li[@class='filter']");
+    var forthLocator = By.xpath("//*[starts-with(@class,'button')]");
+    var fifthLocator = By.xpath("//*[text()='Подписаться сейчас']");
+    var sixthLocator = By.xpath("//img[@class='centered']");
+    var seventhLocator = By.xpath("//div[@class='span4 price-column animated fadeInDown'][1]/following-sibling::*//li[@class='price']");
+    var eightsLocator = By.xpath("//input[@class='span12']");
+
+    // задание 2
+
+    var firstLocator = By.xpath("(//span[@class='baseCard__title'])[5]");// или "//*[text()='Алфавит для взрослых (обновлённый материал)']"
+    var secondLocator = By.xpath("(//p[text()='7 месяцев'])[last()]");
+    var thirdLocator = By.xpath("//*[@href]/ancestor::div");
+    var forthLocator = By.xpath("(//*[@href]/ancestor::div)[5]");
+    var fifthLocator = By.xpath("//div[@class='pageCreate__title']/ancestor::*");
+
+    // задание 3
+
+    var firstLocator = By.xpath("//a[@test-info='about-us']");
+    var secondLocator = By.xpath("//li/a[text()='Бестселлеры']");
+    var thirdLocator = By.xpath("//*[@id='search-input']");
+    var forthLocator = By.xpath("//div[@id='total']");
+    var fifthLocator = By.xpath("//*[text()='Ваш заказ: ']");
+    var sixthLocator = By.xpath("//button[@class='filter-button']");
 
     private WebDriver driver;
 
